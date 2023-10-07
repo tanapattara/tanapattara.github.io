8344a57a25caa34448c3ea748dcb313bc7778ee7
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2023-10-07 22:25:19 +0700

    update navbar

--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -7,60 +7,69 @@ export default function NavigationBar() {
       <nav
         className={`navbar navbar-expand-lg navbar-light bg-light ${styles.navbarModule}`}
       >
-        <a className="navbar-brand" href="#">
-          Tanapattara
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className={`collapse navbar-collapse ml-auto`}
-          id="navbarSupportedContent"
-        >
-          <ul className={`navbar-nav mr-auto ${styles.navbarMenu}`}>
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only"></span>
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Classroom
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
-                  Object Oriented Programming
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Tanapattara
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`collapse navbar-collapse ml-auto`}
+            id="navbarSupportedContent"
+          >
+            <ul className={`navbar-nav mr-auto ${styles.navbarMenu}`}>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home <span className="sr-only"></span>
                 </a>
-                <a className="dropdown-item" href="#">
-                  Frontend Web Development
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  data-bs-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Classroom
                 </a>
-                <a className="dropdown-item" href="#">
-                  Native Mobile Development
+                <ul className="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Object Oriented Programming
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Frontend Web Development
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Native Mobile Development
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="#">
+                  About
                 </a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-          </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
