6e2e010a8dc63291094590e05277956aa68b762e
Tanapattara Wongkhamchan <coach.tpr@gmail.com>
2024-07-15 16:15:00 +0700

    upgrade next version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import "./globals.css";
-import "bootstrap/dist/css/bootstrap.css";
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
-import NavigationBar from "./components/navigation/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,23 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <NavigationBar />
         <div className="container">{children}</div>
-        <Script
-          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-          crossOrigin="anonymous"
-        ></Script>
-        <Script
-          src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-          crossOrigin="anonymous"
-        ></Script>
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-          crossOrigin="anonymous"
-        ></Script>
       </body>
     </html>
   );
