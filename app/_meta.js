aed769914d4aa404a53f14bf9b840d8afbbafc90
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2026-04-22 14:45:55 +0700

    update course

--- app/_meta.js
+++ app/_meta.js
@@ -1,8 +1,14 @@
+import { type } from "node:os";
+
 export default {
   index: "Welcome",
+  git: "Git",
   csharp: "C#",
   oop: "Object-Oriented Programming",
+  dart: "Dart",
   flutter: "Flutter",
+  typescript: "TypeScript",
+  nextjs: "Next.js",
   react_native: "React Native",
   unity2d: "Unity 2D",
   handouts: "Handouts",
