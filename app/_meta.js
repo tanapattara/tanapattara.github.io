8c51bb78a70e6f5f4cace4cc6b81c36438b66032
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2026-04-22 11:34:47 +0700

    innit index page

--- app/_meta.js
+++ app/_meta.js
@@ -1,4 +1,9 @@
 export default {
   index: "Welcome",
+  csharp: "C#",
+  oop: "Object-Oriented Programming",
+  flutter: "Flutter",
+  react_native: "React Native",
+  unity2d: "Unity 2D",
   handouts: "Handouts",
 };
