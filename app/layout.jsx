5b48dcf0ce655c793a9874ce90d435e4256d51ec
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2026-05-07 14:07:46 +0700

    add button

--- app/layout.jsx
+++ app/layout.jsx
@@ -36,6 +36,19 @@ export default async function RootLayout({ children }) {
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        <Script
+          src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+          data-name="bmc-button"
+          data-slug="tanapattara"
+          data-color="#FFDD00"
+          data-emoji=""
+          data-font="Cookie"
+          data-text="Buy me a coffee"
+          data-outline-color="#000000"
+          data-font-color="#000000"
+          data-coffee-color="#ffffff"
+          strategy="lazyOnload"
+        />
         <Layout
           navbar={navbar}
           pageMap={await getPageMap()}
